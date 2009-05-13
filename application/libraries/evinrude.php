0d37a288232b29f98042059c0b863092cfc9a288
Mic@Work <m@micz.it>
2009-05-13 14:50:27 +0200

    version number updated

--- application/libraries/evinrude.php
+++ application/libraries/evinrude.php
@@ -14,7 +14,7 @@ class Evinrude
 {
   private $CI;
   private $template;
-  private $version='1.1-working';
+  private $version='1.1-plugins-working';
   private $basepath;
   var $current_content;
   var $file_last_mod_date;
