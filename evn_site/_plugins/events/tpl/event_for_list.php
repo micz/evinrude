c144b7b7fc488ab05edfc65b18a35d67fd95148a
mic <m@micz.it>
2009-06-05 13:46:58 +0200

    events plugin: minor modifications

--- evn_site/_plugins/events/tpl/event_for_list.php
+++ evn_site/_plugins/events/tpl/event_for_list.php
@@ -1,6 +1,5 @@
-<h3><?=$title?></h3>
+<h3><?=$title?>&nbsp;&nbsp;<span style="font-size:11px;"><a href="<?=base_url().$this->base_path.'/'.$item_permalink?>" title="permalink">#</a></span></h3>
 <p><b><?=$date?><br/>
 <?=$location?></b></p><p>
 </p><p><?=$desc?></p>
 <?if(trim($dl_link)!=''){?><p><a href="<?=$dl_link?>" title="<?=$this->config['download_title_text']?>"> <?=$this->config['download_anchor_text']?></a><?}?>
-<p><a href="<?=base_url().$this->base_path.'/'.$item_permalink?>">permalink</a></p>
