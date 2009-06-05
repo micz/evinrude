ca4808e4b24909256a32d5b304b7b3d008ef2fd4
mic <m@micz.it>
2009-06-05 23:00:08 +0200

    version number set to 1.2-admin-working

--- application/libraries/evinrude.php
+++ application/libraries/evinrude.php
@@ -12,7 +12,7 @@
 
 class Evinrude
 {
-  private $version='1.2dev';
+  private $version='1.2-admin-working';
   private $plugin_autoload_file='_autoload.php';
   private $plugin_main_file='main.php';
 
