177cd6390bcd6ad03ce4d9be232cd20ead456c8f
mic <m@micz.it>
2011-07-16 21:59:02 +0200

    version numbero changed to 1.2dev-admin

--- application/libraries/evinrude.php
+++ application/libraries/evinrude.php
@@ -12,7 +12,7 @@
 
 class Evinrude
 {
-  private $version='1.2-admin-working';
+  private $version='1.2dev-admin';
   private $plugin_autoload_file='_autoload.php';
   private $plugin_main_file='main.php';
 
